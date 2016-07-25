842db1f76afcdf94c0205b8415f3caf56f013303
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-07-25 16:54:39 +0200

    Add drm prop

--- device.mk
+++ device.mk
@@ -260,6 +260,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.min_freq_0=384000
     ro.min_freq_4=384000
 
+# drmservice prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
 
